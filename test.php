84ae4bb8b00611769f25e315842240cc16ddddb3
geitjoch <101874339+u200bpl@users.noreply.github.com>
2022-09-18 22:59:52 +0200

    Added more icons to hero background

--- test.php
+++ test.php
@@ -23,6 +23,26 @@
                         <i class="fa-solid fa-filter"></i>
                         <i class="fa-solid fa-network-wired"></i>
                         <i class="fa-solid fa-sitemap"></i>
+                        <i class="fa-solid fa-code"></i>
+                        <i class="fa-solid fa-gamepad"></i>
+                        <i class="fa-solid fa-splotch"></i>
+                        <i class="fa-solid fa-shield"></i>
+                        <i class="fa-solid fa-code-branch"></i>
+                        <i class="fa-solid fa-gamepad"></i>
+                        <i class="fa-solid fa-cubes"></i>
+                        <i class="fa-solid fa-bug"></i>
+                        <i class="fa-solid fa-gears"></i>
+                        <i class="fa-solid fa-microchip"></i>
+                        <i class="fa-solid fa-diagram-project"></i>
+                        <i class="fa-solid fa-code-commit"></i>
+                        <i class="fa-solid fa-box-archive"></i>
+                        <i class="fa-solid fa-ghost"></i>
+                        <i class="fa-solid fa-circle-nodes"></i>
+                        <i class="fa-solid fa-laptop-code"></i>
+                        <i class="fa-solid fa-folder-open"></i>
+                        <i class="fa-solid fa-filter"></i>
+                        <i class="fa-solid fa-network-wired"></i>
+                        <i class="fa-solid fa-sitemap"></i>
                     </div>
                 <?php } ?>
             </div>
